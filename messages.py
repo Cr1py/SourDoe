8d3ce91195102c777efdf77c4b407b60c56b471c
Cr1py <92011184+Cr1py@users.noreply.github.com>
2022-01-23 12:25:56 -0800

    Update messages.py

--- messages.py
+++ messages.py
@@ -3,13 +3,13 @@ from twilio.rest import Client
 
 # if you have a Twilio account, insert your SID and Auth Token here
 # authors' personal credentials have replaced
-account_sid = 'AC993851487540e81bb6a879b9f2a085f7'
-auth_token = '6b43d9c07001250059ded430648469e2'
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 client = Client(account_sid, auth_token)
 
 client.messages.create(
                      body="https://www.figma.com/proto/dosS0tkqBeAAKR0OUs4Zht/SOURDOE?node-id=3%3A142&scaling=scale-down&page-id=0%3A1&starting-point-node-id=2%3A2",
                      from_= '+19592144471',
-                     to= '+16049708617'
+                     to= os.environ.get('CELL_PHONE_NUMBER')
                  )
 
